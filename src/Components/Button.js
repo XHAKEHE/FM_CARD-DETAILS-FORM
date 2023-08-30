87ac805449f8d9bd5f2bb6bac83f7909690a6d32
Patryk <patryk.cielen@gmail.com>
2023-08-30 19:05:20 +0200

    Finish page started

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,25 +3,39 @@ import classes from './Button.module.css'
 import inputContext from './input-context'
 const Button = props => {
 	const ctx = useContext(inputContext)
-	const [feedback, setFeedback] = useState({ dateMonth: '', number: '' })
+	const [feedback, setFeedback] = useState({start: true})
 
 	const submitHandler = e => {
 		e.preventDefault()
-		// console.log(ctx.inputData)
+		// console.log(ctx.inputData.cvc.length)
 
-		setFeedback({ dateMonth: '', number: '' })
+		setFeedback({ name: '', cvc: '', dateYear: '', dateMonth: '', number: '' })
+
+		if (ctx.inputData.name.length === 0) {
+			setFeedback(prevState => ({ ...prevState, name: 'Cannot be empty!' }))
+		}
+
 		const regex = /[a-zA-Z]/
-		if (regex.test(ctx.inputData.number) || Number(ctx.inputData.number) < 16) {
-			setFeedback(prevState => ({ ...prevState, number: 'Wrong number!' }))
-		}
-		if (Number(ctx.inputData.dateMonth) > 12 || Number(ctx.inputData.dateMonth) === 0) {
-			setFeedback(prevState => ({ ...prevState, dateMonth: 'Wrong date!' }))
+		if (ctx.inputData.number.length < 16) {
+			setFeedback(prevState => ({ ...prevState, number: 'Not enough digits for a card number!' }))
+		} else {
+			if (regex.test(ctx.inputData.number)) {
+				setFeedback(prevState => ({ ...prevState, number: 'Invalid characters in your number!' }))
+			}
 		}
 
-		// console.log(regex.test(regex.test(ctx.inputData.number) || Number(ctx.inputData.number) < 16))
-		// console.log(Number(ctx.inputData.dateMonth) > 12 || Number(ctx.inputData.dateMonth) === 0)
+		if (Number(ctx.inputData.dateMonth) > 12 || ctx.inputData.dateMonth.length < 2) {
+			setFeedback(prevState => ({ ...prevState, dateMonth: 'Invalid month!' }))
+		}
+		if (ctx.inputData.dateYear.length < 2) {
+			setFeedback(prevState => ({ ...prevState, dateYear: 'Invalid year!' }))
+		}
+
+		if (ctx.inputData.cvc.length < 3) {
+			setFeedback(prevState => ({ ...prevState, cvc: 'Invalid CVC!' }))
+		}
+		// console.log(feedback)
 	}
-
 	props.onSubmit(feedback)
 	return <button onClick={submitHandler}>Confirm</button>
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,47 +4,75 @@ import classes from './Form.module.css'
 import { useState } from 'react'
 
 const Form = () => {
-	const [feedback, setFeedback] = useState({ number: '', dateMonth: '' })
+	const [feedback, setFeedback] = useState({ start: true })
+	const [formCompleted, setFormCompleted] = useState(false)
 	const onSubmitBtn = btnFeedback => {
 		setFeedback(btnFeedback)
+		if (Object.values(feedback).every(value => value === '')) {
+			setFormCompleted(true)
+		} else {
+			setFormCompleted(false)
+		}
+		// console.log(feedback)
 	}
-	return (
-		<form className='App-content'>
-			<div className={`${classes['row-container']} ${classes.full}`}>
-				<Input id='name' label='CARDHOLDER NAME' placeholder='e.g. Jane Appleseed' errorFeedback=''></Input>
-			</div>
-			<div className={`${classes['row-container']} ${classes.full}`}>
-				<Input
-					id='number'
-					label='CARD NUMBER'
-					maxLength='16'
-					placeholder='e.g. 1234 5678 9012 3456'
-					errorFeedback={feedback.number}
-				></Input>
-			</div>
+	console.log(formCompleted)
 
-			<div className={`${classes['row-container']} ${classes.full}`}>
-				<div className={`${classes['row-container']} ${classes.half}`}>
-					<div className={`${classes['row-container']} ${classes.half}`}>
+	const content = () => {
+		if (formCompleted) {
+			return <p>siemanko</p>
+		} else {
+			return (
+				<form className='App-content'>
+					<div className={`${classes['row-container']} ${classes.full}`}>
 						<Input
-							id='date-month'
-							label='EXP DATE'
-							maxLength='2'
-							placeholder='MM'
-							errorFeedback={feedback.dateMonth}
+							id='name'
+							label='CARDHOLDER NAME'
+							placeholder='e.g. Jane Appleseed'
+							errorFeedback={feedback.name}
 						></Input>
 					</div>
-					<div className={`${classes['row-container']} ${classes.half}`}>
-						<Input id='date-year' label='(MM/YY)' maxLength='2' placeholder='YY'></Input>
+					<div className={`${classes['row-container']} ${classes.full}`}>
+						<Input
+							id='number'
+							label='CARD NUMBER'
+							maxLength='16'
+							placeholder='e.g. 1234 5678 9012 3456'
+							errorFeedback={feedback.number}
+						></Input>
 					</div>
-				</div>
-				<div className={`${classes['row-container']} ${classes.half}`}>
-					<Input id='cvc' label='CVC' maxLength='3' placeholder='e.g. 123'></Input>
-				</div>
-			</div>
 
-			<Button onSubmit={onSubmitBtn}></Button>
-		</form>
-	)
+					<div className={`${classes['row-container']} ${classes.full}`}>
+						<div className={`${classes['row-container']} ${classes.half}`}>
+							<div className={`${classes['row-container']} ${classes.half}`}>
+								<Input
+									id='date-month'
+									label='EXP DATE'
+									maxLength='2'
+									placeholder='MM'
+									errorFeedback={feedback.dateMonth}
+								></Input>
+							</div>
+							<div className={`${classes['row-container']} ${classes.half}`}>
+								<Input
+									id='date-year'
+									label='(MM/YY)'
+									maxLength='2'
+									placeholder='YY'
+									errorFeedback={feedback.dateYear}
+								></Input>
+							</div>
+						</div>
+						<div className={`${classes['row-container']} ${classes.half}`}>
+							<Input id='cvc' label='CVC' maxLength='3' placeholder='e.g. 123' errorFeedback={feedback.cvc}></Input>
+						</div>
+					</div>
+
+					<Button onSubmit={onSubmitBtn}></Button>
+				</form>
+			)
+		}
+	}
+
+	return content()
 }
 export default Form
